786efd3579ef95463a5bf4ce733528dacf0377ba
maushook <amaushook@tnsinc.com>
2022-06-15 17:52:15 +0200

    commented-return-v0.0-test

--- app/main.py
+++ app/main.py
@@ -52,7 +52,7 @@ def webhook_whatsapp():
     if request.method == "GET":
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return request.args.get('hub.challenge')
-        return "Authentication Failed: Token is Invalid"
+        #return "Authentication Failed: Token is Invalid"
 
     client = WhatsAppWrapper()
     response = client.process_webhook_notification(request.get_json())
